336567dac1d5762dfa436cfabe418699317bc4db
Nils Rothenbuehler <r02nils@gmail.com>
2023-07-28 18:16:59 +0200

    updated sidebar

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -34,20 +34,30 @@
             ]
         }
     ];
+
+    function openPage(page) {
+        window.location.href = page;
+    }
+
 </script>
 
-<div class="flex flex-col justify-between bg-gray-800 w-96 h-[100%] border-t-4 border-green-500">
+<div class="flex flex-col justify-between bg-gray-800 w-72 h-[100%] border-t-4 border-green-500">
     <div class="flex-row">
-        <div class="p-5 border-b-2">
-            <a class="text-white font-bold" href="/"><i class="fa-solid fa-house mr-3"></i>Home</a>
-        </div>
+        <button class="p-5 border-b-2 hover:bg-white hover:bg-opacity-10 w-full text-start text-white font-bold" on:click={() => openPage("/")}>
+            <i class="fa-solid fa-home mr-3"></i>Home
+        </button>
         {#each myProjects as project}
-            <div class="pl-5 pt-5 pr-5">
-                <a class="text-white font-bold" href="/project/{project.id}"><i class="fa-solid fa-list mr-3"></i>{project.name}</a>
-            </div>
+            <button class="p-3 pl-5 pr-5 hover:bg-white hover:bg-opacity-10 w-full text-start text-white font-bold" on:click={() => openPage("/project/" + project.id)}>
+                <i class="fa-solid fa-list mr-3"></i>{project.name}
+            </button>
         {/each}
+        <div class="pl-5 pt-5 pr-5 ">
+            <button class="border-2 p-3 pl-5 rounded-md hover:bg-white hover:bg-opacity-10 w-full text-start text-white font-bold" on:click={() => openPage("/project/new")}>
+                <i class="fa-solid fa-plus mr-3"></i>New Project
+            </button>
+        </div>
     </div>
-    <div class="p-5 border-t-2">
-        <a class="text-white font-bold" href="/settings"><i class="fa-solid fa-gear mr-3"></i>Settings</a>
-    </div>
+    <button class="p-5 border-t-2 hover:bg-white hover:bg-opacity-10 w-full text-start text-white font-bold" on:click={() => openPage("/settings")}>
+        <i class="fa-solid fa-gear mr-3"></i>Settings
+    </button>
 </div>
